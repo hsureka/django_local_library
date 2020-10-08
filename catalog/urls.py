aa1d4a9f7f16e1f6f59665c88e120fa85fdaaa51
Harshit Sureka <hsureka@iitg.ac.in>
2020-10-08 22:09:19 +0530

    First version of application moved into github

---
+++ catalog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('book/', views.BookListView.as_view(), name='books'),
+	path('author/', views.AuthorListView.as_view(), name='authors'),
+	path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+	path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+	path('mybooks/', views.LoanedBooksByUserListView.as_view() , name='my-borrowed'),
+	path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+	path('booksborrowed/', views.BooksBorrowedByUsersListView.as_view(), name='books-borrowed'),
+	path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+]
---
+++ catalog/views.py
@@ -0,0 +1,148 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from catalog.models import Book, Author, Genre, BookInstance, Language
+from django.contrib.auth.mixins import LoginRequiredMixin
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from catalog.forms import RenewBookModelForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+def index(request):
+	"""View function for home page of site."""
+
+	# Generate counts of some of the main objects
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+
+	# Available books (status = 'a')
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+	# The 'all()' is implied by default.
+	num_authors = Author.objects.count()
+	num_genres = Genre.objects.count()
+	num_languages = Language.objects.count()
+	# Number of visits to this view, as counted in the session variable.
+	num_visits = request.session.get('num_visits', 0)
+	request.session['num_visits'] = num_visits + 1
+
+	context = {
+		'num_books' : num_books,
+		'num_instances' : num_instances,
+		'num_instances_available' : num_instances_available,
+		'num_authors' : num_authors,
+		'num_visits' : num_visits,
+		'num_languages' : num_languages,
+		'num_genres' : num_genres,
+	}
+
+	# Render the HTML template index.html with the data in the context variable
+	return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+	model = Book
+	template_name = 'catalog/book_list.html'
+	context_object_name = 'my_book_list' # your own name for the list as a template variable
+	paginate_by = 10
+
+	def get_queryset(self):
+		return Book.objects.all() # Get 5 books containing the title war
+
+	"""def get_context_data(self, **kwargs):
+		# Call the base implementation first to get the context
+		context = super(BookListView, self).get_context_data(**kwargs)
+		# Create any data and add it to the context
+		context['some_data'] = 'This is just some data'
+		return context
+	"""
+
+class AuthorListView(generic.ListView):
+	model = Author
+	template_name = 'catalog/author_list.html'
+	context_object_name = 'author_list'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return Author.objects.all()
+
+class BookDetailView(generic.DetailView):
+	model=Book
+
+class AuthorDetailView(generic.DetailView):
+	model=Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+	"""Generic class-based view listing books on loan to current user."""
+	model = BookInstance
+	template_name = 'catalog/bookinstance_list_borrowed_user.html'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class BooksBorrowedByUsersListView(LoginRequiredMixin, generic.ListView):
+	model = BookInstance
+	template_name = 'catalog/books_boorowed_by_users.html'
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+	book_instance = get_object_or_404(BookInstance,pk=pk)
+
+	# If this is a POST request then process the Form data
+	if request.method == 'POST':
+
+		# Create a form instance and populate it with data from the request (binding):
+		form = RenewBookModelForm(request.POST)
+
+		#Check if the from is valid
+		if form.is_valid():
+			# process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+			book_instance.due_back = form.cleaned_data['due_back']
+			book_instance.save()
+
+			#redirect to a new url
+			return HttpResponseRedirect(reverse('books-borrowed'))
+
+		# If this is a GET (or any other method) create the default form.
+	else:
+		proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+		form = RenewBookModelForm(initial={'renewal_date': proposed_renewal_date})
+
+	context = {
+		'form' : form,
+		'book_instance' : book_instance,
+	}
+
+	return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+	model = Author
+	fields = '__all__'
+	initial = {'date_of_death': '05/01/2018'}
+
+class AuthorUpdate(UpdateView):
+	model =Author
+	fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+	model = Author
+	success_url = reverse_lazy('authors')
+
+class BookCreate(CreateView):
+	model = Book
+	fields = '__all__'
+
+class BookUpdate(UpdateView):
+	model =Book
+	fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
+
+class BookDelete(DeleteView):
+	model = Book
+	success_url = reverse_lazy('books')
